221c7d81298f3c5f01bd1234d1d08ef13ec5f935
JacKC-s <163896934+JacKC-s@users.noreply.github.com>
2026-06-18 19:33:18 -0400

    Added Documentation sources, finished gpio header file

--- hal/gpio.c
+++ hal/gpio.c
@@ -2,6 +2,35 @@
 #include "task.h"
 #include "stm32f446xx.h"
 #include <stdio.h>
+#include "gpio.h"
 
-// I am a bit confused and spent 4 hours working on the rest of the setup 
-// so I am going to give it a break for now
+void GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_PinConfig_t *Config) {
+    // Get pin
+    uint8_t pin = Config->Pin;
+
+    // Set mode
+    GPIOx->MODER &= ~(3UL << (pin * 2)); // multipled by two because two bit register. 
+    // The 3UL is just 3 in binary being bitshifted 
+    GPIOx->MODER |= ((uint32_t)Config->Mode << (pin * 2));
+
+    // Set output type
+    GPIOx->OTYPER &= ~(1UL << pin); // 1UL because only one bit
+    GPIOx->OTYPER |= ((uint32_t)Config->OType << pin);
+
+    // Set Output Speed
+    GPIOx->OSPEEDR &= ~(3UL << (pin * 2));
+    GPIOx->OSPEEDR |= ((uint32_t)Config->Speed << (pin * 2));
+
+    // Set Push Pull -> usually none
+    GPIOx->PUPDR &= ~(3UL << (pin * 2));
+    GPIOx->PUPDR |= ((uint32_t)Config->Pull << (pin * 2));
+
+    // Setting Alternate Function
+    if (Config->Mode == GPIO_MODE_AF) {
+        // To find if it is high or low register
+        uint8_t afr_index = pin >> 3; // This is really cool! Divides by 8. 1000 (8) -> 0001 (1)!
+        uint8_t // Continue here
+    }
+
+}
+
---
+++ hal/gpio.h
@@ -0,0 +1,80 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include "stm32f446xx.h"
+
+// Not all pins will be available for each Port
+
+#define GPIO_PIN_0   0
+#define GPIO_PIN_1   1
+#define GPIO_PIN_2   2
+#define GPIO_PIN_3   3
+#define GPIO_PIN_4   4
+#define GPIO_PIN_5   5
+#define GPIO_PIN_6   6
+#define GPIO_PIN_7   7
+#define GPIO_PIN_8   8
+#define GPIO_PIN_9   9
+#define GPIO_PIN_10  10
+#define GPIO_PIN_11  11
+#define GPIO_PIN_12  12
+#define GPIO_PIN_13  13
+#define GPIO_PIN_14  14
+#define GPIO_PIN_15  15
+
+// Output mode
+typedef enum {
+    GPIO_MODE_INPUT = 0x00,
+    GPIO_MODE_OUTPUT = 0x01,
+    GPIO_MODE_AF = 0x02,
+    GPIO_MODE_ANALOG = 0x04
+} GPIO_MODE_t;
+
+
+// Output type
+typedef enum {
+    GPIO_OTYPE_PP = 0, // Push pull
+    GPIO_OTYPE_OD = 1 // Open drain
+} GPIO_OTYPE_t;
+
+
+// Output speed
+typedef enum {
+    GPIO_SPEED_LOW = 0x00,
+    GPIO_SPEED_MED = 0x01,
+    GPIO_SPEED_FAST = 0x02,
+    GPIO_SPEED_HIGH = 0x03
+} GPIO_SPEED_t;
+
+// Output pull-up/pull down
+typedef enum {
+    GPIO_PUPD_NONE = 0x00,
+    GPIO_PUPD_PU = 0x01,
+    GPIO_PUPD_PD = 0x02,
+    GPIO_PUPD_R = 0x03 
+} GPIO_PUPD_t;
+
+// Bit set reset
+typedef enum {
+    GPIO_PIN_RESET = 0,
+    GPIO_PIN_SET = 1
+} GPIO_STATE_t;
+
+// Configuration for the pin
+typedef struct {
+    uint8_t Pin;  
+    uint8_t Mode;
+    uint8_t OType;
+    uint8_t Speed;
+    uint8_t Pull;
+    uint8_t State;
+    uint8_t AF_Selection;
+} GPIO_PinConfig_t;
+
+// Function Definitions
+void GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_PinConfig_t *Config);
+void GPIO_WritePin(GPIO_TypeDef *GPIOx, uint8_t Pin, GPIO_STATE_t State);
+void GPIO_TogglePin(GPIO_TypeDef *GPIOx, uint8_t Pin);
+uint8_t GPIO_ReadPin(GPIO_TypeDef *GPIOx, uint8_t Pin);
+
+#endif
