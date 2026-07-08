cf83712d92c9548a30c92f07a8c155da1d5bfc36
Joseph Pedicone <josephpedicone06@gmail.com>
2026-07-08 16:35:20 -0400

    {Created the inverted function of the GPIO configuration}

--- hal/gpio.c
+++ hal/gpio.c
@@ -4,12 +4,12 @@
 #include "task.h"
 #include <stdio.h>
 
-void GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_PinConfig_t *Config) {
+void GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_PinConfig_t *Config) { //Function initiator
   // Enables GPIO clocks
   if (GPIOx == GPIOA) {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-  } else if (GPIOx == GPIOB) {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; 
+  } else if (GPIOx == GPIOB) { 
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; 
   } else if (GPIOx == GPIOC) {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
   } else if (GPIOx == GPIOD) {
@@ -26,7 +26,7 @@ void GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_PinConfig_t *Config) {
   }
 
   // Get pin
-  uint8_t pin = Config->Pin;
+  uint8_t pin = Config->Pin; //unsigned 8-bit integer from 0 to 255, 15 per GPIO
 
   // Set mode
   GPIOx->MODER &= ~(3UL << (pin * 2)); // multipled by two because two bit register.
@@ -38,7 +38,7 @@ void GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_PinConfig_t *Config) {
   GPIOx->OTYPER |= ((uint32_t)Config->OType << pin);
 
   // Set Output Speed
-  GPIOx->OSPEEDR &= ~(3UL << (pin * 2));
+  GPIOx->OSPEEDR &= ~(3UL << (pin * 2)); //undo or invert
   GPIOx->OSPEEDR |= ((uint32_t)Config->Speed << (pin * 2));
 
   // Set Push Pull -> usually none
@@ -75,5 +75,52 @@ void GPIO_TogglePin(GPIO_TypeDef *GPIOx, uint8_t pin) {
 // I dont know if this properly works, will try it soon. -> Should work
 uint8_t GPIO_ReadPin(GPIO_TypeDef *GPIOx, uint8_t Pin) {
   return ((GPIOx->IDR & (1UL << Pin)) != 0) ? 1 : 0;
+}
+void GPIO_DeInit(GPIO_TypeDef *GPIOx, GPIO_PinConfig_t *Config) { //Function initiator
+  // Enables GPIO clocks
+  if (GPIOx == GPIOA) {
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOAEN); //& ~ (RCC_....)
+  } else if (GPIOx == GPIOB) { //assignment is to make an invert of the  
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOBEN); //Function goes down the list enabling settings
+  } else if (GPIOx == GPIOC) {
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOCEN);
+  } else if (GPIOx == GPIOD) {
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIODEN);
+  } else if (GPIOx == GPIOE) {
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOEEN);
+  } else if (GPIOx == GPIOF) {
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOFEN);
+  } else if (GPIOx == GPIOG) {
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOGEN);
+  } else if (GPIOx == GPIOH) {
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOHEN);
+  } else {
+  }
+
+  // Get pin
+  uint8_t pin = Config->Pin; //unsigned 8-bit integer from 0 to 255, 15 per GPIO
+
+  GPIOx->MODER &= ~(3UL << (pin * 2)); 
+  
+  // Reset output type
+  GPIOx->OTYPER &= ~(1UL << pin); // 1UL because only one bit
+
+  // Reset Output Speed
+  GPIOx->OSPEEDR &= ~(3UL << (pin * 2)); //undo or invert
+
+  // Reset Push Pull -> usually none
+  GPIOx->PUPDR &= ~(3UL << (pin * 2));
+  
+  // Resetting Alternate Function
+  if (Config->Mode == GPIO_MODE_AF) {
+    // To find if it is high or low register
+    uint8_t afr_index =
+        pin >> 3; // This is really cool! Divides by 8. 1000 (8) -> 0001 (1)!
+    uint8_t afr_shift = (pin & 0x07)
+                        << 2; // Another cool trick that equals (pin % 8) * 4
+
+    GPIOx->AFR[afr_index] &=
+        ~(0xFUL << afr_shift); // Clears the 4 bits where the afr is
+  }
 }
 // ADD READ WHEN USB COMMUNICATION WORKS
